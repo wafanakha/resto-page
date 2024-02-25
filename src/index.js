60e6e609087fc06c67059229ce99265674663cae
wafanakha <wafanakha15@gmail.com>
2024-02-25 11:24:09 +0700

    add home modukes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import _ from "lodash";
 import "./style.css";
 
+import { home } from "./home.js";
+
 const content = document.getElementById("content");
 
 const menu = document.createElement("div");
 content.appendChild(menu);
 
-const footer = document.createElement("footer");
 content.insertAdjacentHTML("afterend", "<footer></footer>");
+
+home();
