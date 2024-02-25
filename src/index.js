e7bcac752a73fa1eb8eceb896dc123806a470703
wafanakha <wafanakha15@gmail.com>
2024-02-25 10:33:09 +0700

    add html element

--- src/index.js
+++ src/index.js
@@ -7,4 +7,4 @@ const menu = document.createElement("div");
 content.appendChild(menu);
 
 const footer = document.createElement("footer");
-content.appendChild(footer);
+content.insertAdjacentHTML("afterend", "<footer></footer>");
