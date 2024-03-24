9fbf016a3521ff61d4f01a8b97cab3c85759b594
wafanakha <wafanakha15@gmail.com>
2024-03-24 14:49:37 +0700

    idk

--- src/home.js
+++ src/home.js
@@ -1,10 +1,26 @@
 export const home = () => {
   const title = document.createElement("div");
   title.classList.add("title");
+  const titleName = document.createElement("h2");
+  const titileName_text = document.createTextNode("TITLE HERE");
+
   const menu = document.querySelector(".menu");
 
   const info = document.createElement("div");
   info.classList.add("info");
+
+  const about = document.createElement("div");
+  const aboutparag = document.createElement("p");
+  const aboutparag_text = document.createTextNode(
+    "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia adipisci officia, blanditiis a corrupti ducimus deserunt suscipit laboriosam voluptatem nisi?"
+  );
+
+  aboutparag.appendChild(aboutparag_text);
+  about.appendChild(aboutparag);
+  info.appendChild(about);
+
+  titleName.appendChild(titileName_text);
+  title.appendChild(titleName);
 
   menu.appendChild(title);
   menu.appendChild(info);
